538032593b0e2582a0eccf96a980d745c57123ca
Oskar Kwasniewski <okwasniewski@macbook-pro-oskar.home>
2020-03-16 15:22:34 +0100

    menu component

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -4,18 +4,11 @@ import { NavLink } from 'react-router-dom';
 function Header(props) {
     return (
        <header className="top">
-            <div className="top__logo"> Coronawirus </div>
+            <div className="top__logo"> <NavLink to="/"> Coronawirus </NavLink> </div>
             <ul className="top__items">
-                <li>
-                    <NavLink to="/countries"> Countries </NavLink>
-                </li>
-                <li>
-                <NavLink to="/about"> About </NavLink>
-
-                </li>
-                <li>
-                <NavLink to="/countries"> Countries </NavLink>
-                </li>
+                <li className="top__item"> <NavLink to="/countries"> Countries </NavLink></li>
+                <li className="top__item"><NavLink to="/about"> About </NavLink> </li>
+                <li className="top__item"><NavLink to="/countries"> Authors </NavLink> </li>
             </ul>
         </header>
     );
