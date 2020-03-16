06c05f2f4a7e66726e7c5c860ce6c8bd26e75336
Oskar Kwasniewski <okwasniewski@macbook-pro-oskar.home>
2020-03-16 14:29:03 +0100

    header

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -3,10 +3,21 @@ import { NavLink } from 'react-router-dom';
 
 function Header(props) {
     return (
-        <div>
-            {/* tutaj linki navigacja i logo */}
-            header
-        </div>
+       <header className="top">
+            <div className="top__logo"> Coronawirus </div>
+            <ul className="top__items">
+                <li>
+                    <NavLink to="/countries"> Countries </NavLink>
+                </li>
+                <li>
+                <NavLink to="/about"> About </NavLink>
+
+                </li>
+                <li>
+                <NavLink to="/countries"> Countries </NavLink>
+                </li>
+            </ul>
+        </header>
     );
 }
 
