750a7053f8e59c534c1f5790a22977e3ece5e16e
Ryczko <Ryczko@users.noreply.github.com>
2020-03-16 13:48:38 +0100

    base structure

---
+++ src/sites/Countries.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class Countries extends React.Component {
+    state = {}
+    render() {
+        return (
+            <div>
+                kraje
+            </div>
+        );
+    }
+}
+
+export default Countries;
---
+++ src/sites/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class Main extends React.Component {
+    state = {}
+    render() {
+        return (
+            <div>
+                Main
+            </div>
+        );
+    }
+}
+
+export default Main;
