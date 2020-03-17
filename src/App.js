85305424af2a6a7e497146cba0d3520ed4a44fb2
Oskar Kwasniewski <okwasniewski@MacBook-Pro-Oskar.local>
2020-03-17 17:02:59 +0100

    countries

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import './fontello-118af5e6/css/fontello.css';
 import './stylesheets/main.scss';
+import CountryDetail from './sites/CountryDetail';
 import Footer from './layouts/Footer';
 import Header from './layouts/Header';
 import Main from './sites/Main';
@@ -17,9 +18,10 @@ function App() {
       <BrowserRouter>
         <Header />
         <Route path="/" exact component={Main} />
-        <Route path="/countries" component={Countries} />
+        <Route path="/countries" exact component={Countries} />
         <Route path="/about" component={About} />
         <Route path="/authors" component={Authors} />
+        <Route path="/countries/:id" component={CountryDetail}/>
         <Footer/>
       </BrowserRouter>
     </div>
--- src/sites/Countries.js
+++ src/sites/Countries.js
@@ -1,14 +1,20 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 
 
 class Countries extends React.Component {
+
+    state = {
+        isLoaded: false,
+        data: {},
+        recovered: '',
+        cases: '',
+    }
     
 fetchingdata() {
     fetch('https://covid19.mathdro.id/api/confirmed')
     .then(res => res.json())
     .then(data => {
-        console.log(data[0].countryRegion)
         const { confirmed, recovered, deaths } = data;
         this.setState({
             isLoaded: true,
@@ -18,12 +24,8 @@ fetchingdata() {
         })
     })
 }
-    state = {
-        isLoaded: false,
-        data: {},
-        recovered: '',
-        cases: '',
-    }
+
+    
     render() {
         this.fetchingdata()
         if(this.state.isLoaded){
@@ -32,7 +34,7 @@ fetchingdata() {
                    {
                    this.state.data.map(item => {
                        return(<div className="">
-                       <h1>{item.countryRegion}</h1> 
+                       <Link to={`/countries/${item.countryRegion}`}><h1>{item.countryRegion}</h1> </Link>
                        <p>Confirmed: {item.confirmed}</p> 
                        <p>Deaths: {item.deaths}</p> 
                        <p>Recovered: {item.recovered}</p> 
