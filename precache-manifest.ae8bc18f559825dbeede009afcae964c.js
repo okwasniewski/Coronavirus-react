ea246dccef22e331ee60876cd352b76e159ca466
unknown <oskarkwasniewski@icloud.com>
2020-03-24 22:16:54 +0100

    Updates

---
+++ precache-manifest.ae8bc18f559825dbeede009afcae964c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "885b2399598fea462f0b84e07e9bf55f",
+    "url": "/Coronavirus-react/index.html"
+  },
+  {
+    "revision": "65e0b30d08a0c9e7ae7c",
+    "url": "/Coronavirus-react/static/css/main.e2027679.chunk.css"
+  },
+  {
+    "revision": "f2cad7e5b826a47d74ef",
+    "url": "/Coronavirus-react/static/js/2.330a4adc.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/Coronavirus-react/static/js/2.330a4adc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65e0b30d08a0c9e7ae7c",
+    "url": "/Coronavirus-react/static/js/main.20befb4f.chunk.js"
+  },
+  {
+    "revision": "b19e175653534847e37c",
+    "url": "/Coronavirus-react/static/js/runtime-main.e5674265.js"
+  },
+  {
+    "revision": "1b3b5fd57b6c397dfb6e2ccdbbc95ff3",
+    "url": "/Coronavirus-react/static/media/fontello.1b3b5fd5.woff2"
+  },
+  {
+    "revision": "cbb0a12e2e9dfeb7e848f719097125de",
+    "url": "/Coronavirus-react/static/media/fontello.cbb0a12e.ttf"
+  },
+  {
+    "revision": "e45d14a57001544f670ff017bc9e7d85",
+    "url": "/Coronavirus-react/static/media/fontello.e45d14a5.woff"
+  },
+  {
+    "revision": "ef906fb3733225af9218d5f92c307177",
+    "url": "/Coronavirus-react/static/media/fontello.ef906fb3.svg"
+  },
+  {
+    "revision": "f98de043ff999f1feb73681ee6c6806b",
+    "url": "/Coronavirus-react/static/media/fontello.f98de043.eot"
+  }
+]);
